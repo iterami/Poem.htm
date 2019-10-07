2181468800c43fbb0a2f84b7b3c337c5ebad8187
Jan Hořava <czechinthemail@gmail.com>
2019-10-07 13:24:21 -0700

    added core_repo_reset() usage

--- js/main.js
+++ js/main.js
@@ -4,18 +4,16 @@ function repo_init(){
     core_repo_init({
       'events': {
         'generate': {
-          'onclick': function(){
-              generate();
-              core_escape();
-          },
+          'onclick': core_repo_reset,
         },
       },
-      'info': '<input id=generate type=button value="Generate [ENTER]">',
+      'info': '<input id=generate type=button value=Generate>',
       'keybinds': {
         13: {
           'todo': generate,
         },
       },
+      'reset': generate,
       'title': 'Poem.htm',
     });
 
